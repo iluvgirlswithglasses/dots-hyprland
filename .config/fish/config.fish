543de977d088adbc8cad94af3b6bae8edda6eba6
iluvgirlswithglasses <luunamdat204@gmail.com>
2024-05-07 20:43:46 +0700

    terminal configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,54 @@
+
+######################################################################
+#### --------------------- system variables --------------------- ####
+######################################################################
+
+set -x PATH $PATH /usr/sbin ~/.local/bin
+
+
+######################################################################
+#### ---------------------- greeting scene ---------------------- ####
+######################################################################
+
+macchina-linux-x86_64
+
+
+#######################################################################
+#### -------------------------- aliases -------------------------- ####
+#######################################################################
+
+alias py="python3"
+alias vim="nvim"
+alias cin="cat"
+alias q="exit"
+alias :q="exit"
+alias clip="xsel --clipboard"
+
+
+#######################################################################
+#### ------------------ quick directory actions ------------------ ####
+#######################################################################
+
+function qpush
+    git add .
+    git commit -m $argv
+    git push
+end
+
+function pullsh
+    git pull
+    qpush $argv
+end
+
+function yd
+    pwd | xsel --clipboard;
+end
+
+
+#######################################################################
+#### ---------------------- terminal config ---------------------- ####
+#######################################################################
+
 function fish_prompt -d "Write out the prompt"
     # This shows up as USER@HOST /home/user/ >, with the directory colored
     # $USER and $hostname are set by fish, so you can just use them
@@ -6,18 +57,21 @@ function fish_prompt -d "Write out the prompt"
         (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
 end
 
+function postexec_test --on-event fish_postexec
+   echo
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting
-
 end
 
-starship init fish | source
+# starship init fish | source
 if test -f ~/.cache/ags/user/generated/terminal/sequences.txt
     cat ~/.cache/ags/user/generated/terminal/sequences.txt
 end
 
-alias pamcan=pacman
+fish_config prompt choose pythonista
 
 # function fish_prompt
 #   set_color cyan; echo (pwd)
