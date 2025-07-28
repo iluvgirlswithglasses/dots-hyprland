17980516c76b547022cc9ccf61ef7469a4f7b485
iluvgirlswithglasses <luunamdat204@gmail.com>
2025-07-28 14:21:26 +0700

    some coloring

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,58 @@
+
+######################################################################
+#### --------------------- system variables --------------------- ####
+######################################################################
+
+set -x PATH $PATH /usr/sbin ~/.local/bin
+
+abbr --add cloneme git clone ssh+git://git@github.com/iluvgirlswithglasses/
+abbr --add setntp timedatectl set-ntp
+abbr --add settime sudo timedatectl set-time
+
+
+######################################################################
+#### ---------------------- greeting scene ---------------------- ####
+######################################################################
+
+macchina-linux-x86_64
+
+
+#######################################################################
+#### -------------------------- aliases -------------------------- ####
+#######################################################################
+
+alias py="python3"
+alias vim="nvim"
+alias cin="cat"
+alias q="exit"
+alias :q="exit"
+alias clip="wl-copy"
+
+
+#######################################################################
+#### ------------------ quick directory actions ------------------ ####
+#######################################################################
+
+function qpush
+    git add .
+    git commit -m $argv
+    git push
+end
+
+function pullsh
+    git pull
+    qpush $argv
+end
+
+function yd
+    pwd | tr -d '\n' | wl-copy;
+end
+
+
+#######################################################################
+#### ---------------------- terminal config ---------------------- ####
+#######################################################################
+
 function fish_prompt -d "Write out the prompt"
     # This shows up as USER@HOST /home/user/ >, with the directory colored
     # $USER and $hostname are set by fish, so you can just use them
@@ -6,21 +61,21 @@ function fish_prompt -d "Write out the prompt"
         (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
 end
 
+function postexec_test --on-event fish_postexec
+   echo
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting
-
 end
 
-starship init fish | source
-if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
-    cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+# starship init fish | source
+if test -f ~/.cache/ags/user/generated/terminal/sequences.txt
+    cat ~/.cache/ags/user/generated/terminal/sequences.txt
 end
 
-alias pamcan pacman
-alias ls 'eza --icons'
-alias clear "printf '\033[2J\033[3J\033[1;1H'"
-    
+fish_config prompt choose pythonista
 
 # function fish_prompt
 #   set_color cyan; echo (pwd)
